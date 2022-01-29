3fef6412fe0b54446c5ffb3fe859941825bae0b0
Joyce de Fatima <joycecosta453@hotmail.com>
2022-01-29 11:42:58 -0300

    Enviando formulario apenas com HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,43 @@
-const form = document.getElementById('form')
-const email = document.getElementById('email')
-const nomecompleto = document.getElementById('nomecompleto')
-const idade = document.getElementById('idade')
-const telefone = document.getElementById('telefone')
-const usuario = document.getElementById('usuario')
-const senha = document.getElementById('senha')
-const senha2 = document.getElementById('senha2')
+// const form = document.getElementById('form')
+// const email = document.getElementById('email')
+// const nomecompleto = document.getElementById('nomecompleto')
+// const idade = document.getElementById('idade')
+// const telefone = document.getElementById('telefone')
+// const usuario = document.getElementById('usuario')
+// const senha = document.getElementById('senha')
+// const senha2 = document.getElementById('senha2')
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
 
-  chenckInputs();
-});
+//   chenckInputs();
+// });
 
-function chenckInputs() {
-  const emailValue = email.value.trim();
-  const nomeValue = nomecompleto.value.trim();
-  const idadeValue = idade.value.trim();
-  const telefoneValue = telefone.value.trim();
-  const usuarioValue = usuario.value.trim();
-  const senhaValue = senha.value.trim();
-  const senha2Value = senha2.value.trim();
+// function chenckInputs() {
+//   const emailValue = email.value.trim();
+//   const nomeValue = nomecompleto.value.trim();
+//   const idadeValue = idade.value.trim();
+//   const telefoneValue = telefone.value.trim();
+//   const usuarioValue = usuario.value.trim();
+//   const senhaValue = senha.value.trim();
+//   const senha2Value = senha2.value.trim();
 
-  if (emailValue === '') {
-    setErrorFor(email, "Email invalido");
-  } else {
-    setSuccessFor(email);
-  }
-}
+//   if (emailValue === '') {
+//     setErrorFor(email, "Email invalido");
+//   } else {
+//     setSuccessFor(email);
+//   }
+// }
 
-function setErrorFor(input) {
-  const formulario = input.parentElement;
-}
+// function setErrorFor(input) {
+//   const formulario = input.parentElement;
+// }
+
+// if (button_cadastrar == "") {
+//   console.error("Algo deu errado, tente novamente!");
+// } else (button_cadastrar == getElementById(form))
+
+// function button_cadastrar(aviso) {
+//   console.log(aviso)
+// }
+
